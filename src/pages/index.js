80afc673f043f94fd72a156e6c895bf40cd5cd95
Shaariq06 <muhammadalishaariq@gmail.com>
2023-04-09 19:39:00 +0100

    "adding links on homepage"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 
 export default function Home() {
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Welcome to SpaceTraders!</h1>
+      <div className={styles.loginLink}>
+        <p>Have an account?</p>
+        <a href="login" >Login</a>
+      </div>
+      <div className={styles.signupLink}>
+        <p>Are you a new user?</p>
+        <a href="signup" >Signup</a>
+      </div>
     </div>
   )
 }
