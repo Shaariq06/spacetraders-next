241c6f345443bca101aa2e4db8a09ad8dca7d905
Shaariq06 <muhammadalishaariq@gmail.com>
2024-06-07 10:46:21 +0100

    add logout to dash

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,48 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Logout from './logout';
 
 const Dashboard = () => {
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchUserData = async () => {
       const token = localStorage.getItem('authToken');
-      if (token) {
-        try {
-          const response = await fetch('https://api.spacetraders.io/my/account', {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`,
-            },
-          });
-          if (response.ok) {
-            const data = await response.json();
-            setUserData(data);
-          } else {
-            console.error('Error fetching user data');
-          }
-        } catch (error) {
-          console.error('Error fetching user data:', error);
+      if (!token) {
+        router.push('/login');
+        return;
+      }
+
+      try {
+        const response = await fetch('https://api.spacetraders.io/my/account', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setUserData(data);
+        } else {
+          setError('Failed to fetch user data');
+          console.error('Error fetching user data:', response.statusText);
         }
+      } catch (error) {
+        setError('Failed to fetch user data');
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchUserData();
-  }, []);
+  }, [router]);
+
+  if (loading) {
+    return <p>Loading user data...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <div>
       <h1>Dashboard</h1>
-      {userData ? (
+      {userData && (
         <div>
-          <p>Welcome, {userData.username}!</p>
+          <p>Welcome, {userData.user.username}!</p>
           {/* Display other user data */}
         </div>
-      ) : (
-        <p>Loading user data...</p>
       )}
+      <Logout />
     </div>
-  )
+  );
 };
 
 export default Dashboard;
